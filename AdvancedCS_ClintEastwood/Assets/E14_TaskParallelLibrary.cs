1a445ea08e20db1ab6a26359cf50fccb3a66f865
risulkarimx1 <risulkarimx1@gmail.com>
2019-01-17 20:51:09 +0100

    E14_Task Factory

--- AdvancedCS_ClintEastwood/Assets/E14_TaskParallelLibrary.cs
+++ AdvancedCS_ClintEastwood/Assets/E14_TaskParallelLibrary.cs
@@ -9,10 +9,13 @@ public class E14_TaskParallelLibrary : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var task = new Task(() => { GetDataFromServer("x1", 1000); });
+        var t1 = Task.Factory.StartNew(() => GetDataFromServer("x1", 2454));
+        var t2 = Task.Factory.StartNew(() => GetDataFromServer("x2", 2344));
+        var t3 = Task.Factory.StartNew(() => GetDataFromServer("x3", 1354));
+        var t4 = Task.Factory.StartNew(() => GetDataFromServer("x4", 5624));
 
     }
-    public void GetDataFromServer(string taskId,int duration)
+    public void GetDataFromServer(string taskId, int duration)
     {
         Debug.Log($"Starting task {taskId}");
         Thread.Sleep(duration);
